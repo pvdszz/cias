b642d682e0d958bd52ffbd87ff935159e0f9d34f
unknown <diep13111997@gmail.com>
2020-07-20 09:57:26 -0700

    aaa

--- wp-content/themes/theretailer-child/functions.php
+++ wp-content/themes/theretailer-child/functions.php
@@ -245,23 +245,20 @@ function cias_booking_options_product_tab_content()
 				$item->add_meta_data('Trẻ em', $values['wdm_kids']);
 			}
 		}
+		add_action('woocommerce_cart_item_custom', 'woocommerce_cart_item_custom', 10, 4);
+		function woocommerce_cart_item_custom( $products,$cart_item)
+		{
 
-		// add_filter('woocommerce_product_get_price', 'display_super_sale_price', 10, 2);
-		// function display_super_sale_price($price, $product)
-		
-		// {
-		// 	global $adult, $child, $custom_details;
-		// 	$connect = mysqli_connect("localhost", "root", "", "cias_dev");
-		// 	$query = "SELECT * FROM `cias_price_for_each_person` ORDER BY `cias_price_for_each_person`.`ID` ASC LIMIT 1";
-		// 	$result = mysqli_query($connect, $query);
-		// 	while($row = mysqli_fetch_array($result))
-		// 	{
-		// 		$adult = $row["price_for_adult"];
-		// 		$price = $adult * $custom_details;
-		// 		echo $custom_details;
-		
-		// 	}
-		// 	return $price;
+			global $price ;
+			$adult = get_post_meta($products->get_id(), 'price_for_adult', true ) * $cart_item['wdm_adult'];
+			$kids = get_post_meta($products->get_id(), 'price_for_child', true ) * $cart_item['wdm_kids'];
+			$price = ($adult + $kids);
+			return $price;
+		}
+		add_filter('woocommerce_product_get_price', 'woocommerce_custom_price', 10, 2);
+		function woocommerce_custom_price($price, $products){
+			return $price;
+		}
 
 
 			
@@ -281,8 +278,8 @@ function cias_booking_options_product_tab_content()
 			return $fields;
 		}
 
-		add_action('woocommerce_cart_item_custom', 'woocommerce_cart_item_custom', 10, 4);
-		function woocommerce_cart_item_custom($products, $cart_item){
+		// add_action('woocommerce_cart_item_custom', 'woocommerce_cart_item_custom', 10, 4);
+		// function woocommerce_cart_item_custom($products, $cart_item){
 			
 			// echo '<pre></pre><h3>++++++++++++++++++++++</h3>';
 			// // print_r($products->get_id());
@@ -291,10 +288,10 @@ function cias_booking_options_product_tab_content()
 			// echo $cart_item['wdm_kids'] . '<br />';
 			// echo '<pre></pre><h3>++++++++++++++++++++++</h3>';
 
-			$adult = get_post_meta($products->get_id(), 'price_for_adult', true ) * $cart_item['wdm_adult'];
-			$kids = get_post_meta($products->get_id(), 'price_for_child', true ) * $cart_item['wdm_kids'];
-			echo 'Total: ' . ($adult + $kids);
-		}
+		// 	$adult = get_post_meta($products->get_id(), 'price_for_adult', true ) * $cart_item['wdm_adult'];
+		// 	$kids = get_post_meta($products->get_id(), 'price_for_child', true ) * $cart_item['wdm_kids'];
+		// 	echo 'Total: ' . ($adult + $kids);
+		// }
 
 		add_action( 'woocommerce_checkout_create_order', 'change_total_on_checkings', 20, 1 );
 		function change_total_on_checkings( $order ) {
