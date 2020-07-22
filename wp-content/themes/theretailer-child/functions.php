8beeeb14d0efd21d7e0408323a635aaa2e1b9b64
unknown <diep13111997@gmail.com>
2020-07-22 11:57:45 -0700

    update cias

--- wp-content/themes/theretailer-child/functions.php
+++ wp-content/themes/theretailer-child/functions.php
@@ -272,7 +272,49 @@ function cias_booking_options_product_tab_content()
 		}
 		// Remove Product Prices
 		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-
+		function mysite_pending($order_id) {
+			error_log("$order_id set to PENDING", 0);
+			}
+			function mysite_failed($order_id) {
+			error_log("$order_id set to FAILED", 0);
+			}
+			function mysite_hold($order_id) {
+			error_log("$order_id set to ON HOLD", 0);
+			}
+			function mysite_processing($order_id) {
+			error_log("$order_id set to PROCESSING", 0);
+			}
+			function mysite_completed($order_id) {
+				error_log("$order_id set to COMPLETED", 0);
+				$chars = "abcdefghijkmnopqrstuvwxyz023456789"; 
+				srand((double)microtime()*1000000); 
+				$i = 0; 
+				$pass = 'Cias-'.'' ; 
+			
+				while ($i <= 7) { 
+					$num = rand() % 33; 
+					$tmp = substr($chars, $num, 1); 
+					$pass = $pass . $tmp; 
+					$i++; 
+				} 
+			
+				error_log($pass); 
+			}
+			function mysite_refunded($order_id) {
+			error_log("$order_id set to REFUNDED", 0);
+			}
+			function mysite_cancelled($order_id) {
+			error_log("$order_id set to CANCELLED", 0);
+			}
+		
+			add_action( 'woocommerce_order_status_pending', 'mysite_pending');
+			add_action( 'woocommerce_order_status_failed', 'mysite_failed');
+			add_action( 'woocommerce_order_status_on-hold', 'mysite_hold');
+			// Note that it's woocommerce_order_status_on-hold, not on_hold.
+			add_action( 'woocommerce_order_status_processing', 'mysite_processing');
+			add_action( 'woocommerce_order_status_completed', 'mysite_completed');
+			add_action( 'woocommerce_order_status_refunded', 'mysite_refunded');
+			add_action( 'woocommerce_order_status_cancelled', 'mysite_cancelled');
 
 		// }
 		add_filter( 'woocommerce_checkout_fields' , 'cias_remove_billing_fields' );
